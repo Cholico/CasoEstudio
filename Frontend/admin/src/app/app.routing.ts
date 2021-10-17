29ad7ce575d6cf328b744c7e51dbdeb7dcdd6678
Cholico <alejandro.cholico@alumnos.udg.mx>
2021-10-17 00:10:27 -0500

    Html y css del main page

--- Frontend/admin/src/app/app.routing.ts
+++ Frontend/admin/src/app/app.routing.ts
@@ -1,10 +1,13 @@
 import {Routes, RouterModule} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import { InicioComponent } from './components/inicio/inicio.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 
 const appRoute: Routes = [
-  {path: '', component: InicioComponent}
+  {path: '', component: InicioComponent},
+  {path: '/components/sidebar/sidebar.component', component: SidebarComponent}
 ]
+
 
 export const appRoutingPorviders : any[]=[];
 export const routing : ModuleWithProviders<any> = RouterModule.forRoot(appRoute);
